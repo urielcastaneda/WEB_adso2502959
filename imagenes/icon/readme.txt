3c29c3dfaa360078d4ee4fd476187b3e4118b57c
uriel <urielcastaneda@misena.edu.co>
2022-12-08 16:24:17 -0500

    Incorporación index y carpetas de imagenes y css pagina inicial

---
+++ imagenes/icon/readme.txt
@@ -0,0 +1,20 @@
+Paquete de íconos generado con la herramienta: 
+https://realfavicongenerator.net/
+--------------
+Típos y características de íconos en artículo:
+Como incluir correctamente un favicon para múltiples resoluciones
+
+https://diblet.com/blog/como-incluir-correctamente-un-favicon/
+--------------
+código HTML  sugerido por la herramienta   realfavicongenerator:
+----
+    <link rel="apple-touch-icon" sizes="120x120" href="./imagenes/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./imagenes/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./imagenes/icon/favicon-16x16.png">
+    <link rel="manifest" href="./imagenes/icon/site.webmanifest">
+    <link rel="mask-icon" href="./imagenes/icon/safari-pinned-tab.svg" color="#5bbad5">
+----
+en una sola línea se pueden ubicar las descripciones de diferentes tamaños, 
+pero referenciando el archivo .ICO
+
+<link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="DIRECCIÓN DE NUESTRO ARCHIVO .ICO">
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desarrollo WEB - SENA</title>
+    <META HTTP-EQUIV="REFRESH" CONTENT="6;URL=./adso2502959.cundi.co/index.html">
+
+    <!--  Dediniciones ícono favorito --- favicon.ico --->
+    <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="./imagenes/icon/favicon.ico" type="image/x-icon">    
+    <link rel="apple-touch-icon" sizes="120x120" href="./imagenes/icon/apple-touch-icon.png">
+    <link rel="manifest" href="./imagenes/icon/site.webmanifest">
+    <link rel="mask-icon" href="./imagenes/icon/safari-pinned-tab.svg" color="#5bbad5">
+
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+
+   <link rel="stylesheet" href="./css/estilos.css?n=1">
+ 
+  
+</head>
+
+<body>
+
+    <header class="hero">
+        <nav class="nav container">
+            <div>
+                <img src="./imagenes/logo_sena1.jpg" class="intro__img">
+            </div>            
+            <div class="nav__logo">
+                <h2 class="nav__title">Centro de Desarrollo Agroindustrial y Empresarial - CDAE</h2>
+            </div>
+
+
+        </nav>
+
+        <section class="hero__container container">
+            <h2 class="hero__title">Competencia:</h2><br>
+            <h1 class="hero__title"><b>Talleres Desarrollo WEB - SENA</h1>
+            <p class="hero__paragraph">Ejercicios y talleres WEB</b></p>
+            <li><a href="./adso2502959.cundi.co/index.html"class="cta">Empieza</a></li>
+           
+            
+        </section>
+    </header>
+
+    <main>
+       
+    </main>
+
+   
+</body>
+
+</html>
